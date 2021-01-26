deb045d7ffb9089112b5559f2b3a2afd26aa58de
AnnaxGrace <anna.grace.conover@gmail.com>
2021-01-26 16:45:07 -0600

    added check if the seconds left are less than 10 so the display isn't affected

--- dom/timer/timer-dom.js
+++ dom/timer/timer-dom.js
@@ -1,28 +1,31 @@
 // Create a timer using DOM manipulation techniques. The following files are ready to go with some boilerplate code.
 console.log("time to start the timer!");
 
-    const countDown = document.querySelector("p");
-    const btnDiv = document.querySelector("div");
-    let secondsLeft = 60;
+const countDown = document.querySelector("p");
+const btnDiv = document.querySelector("div");
+let secondsLeft = 60;
 
-    btnDiv.children[0].addEventListener("click", function () {
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        countDown.textContent = "0:" + secondsLeft;
-    
-        if(secondsLeft === 0) {
-          clearInterval(timerInterval);
-        }
-    
-      }, 1000);
+btnDiv.children[0].addEventListener("click", function () {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    if (secondsLeft < 10) {
+      countDown.textContent = "0:0" + secondsLeft;
+    } else {
+      countDown.textContent = "0:" + secondsLeft;
+    }
 
-      btnDiv.children[1].addEventListener("click", function () {
-        clearInterval(timerInterval)
-    });
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      secondsLeft = 60;
+    }
+  }, 1000);
+
+  btnDiv.children[1].addEventListener("click", function () {
+    clearInterval(timerInterval);
+  });
 });
 
-
 btnDiv.children[2].addEventListener("click", function () {
-    countDown.textContent = "0:00";
-    secondsLeft = 60;
+  countDown.textContent = "0:00";
+  secondsLeft = 60;
 });
